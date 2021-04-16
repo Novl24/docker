f20583d353f74809a10fbdd6baef110e894eccd2
Novl24 <74337952+Novl24@users.noreply.github.com>
2021-04-16 10:56:46 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -23,7 +23,7 @@ ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-ShellURL=https://gitee.com/novl24/docker
+ShellURL=https://gitee.com/novl24/docker1
 ScriptsURL=https://gitee.com/novl24/impossible1
 
 ## 更新crontab，gitee服务器同一时间限制5个链接，因此每个人更新代码必须错开时间，每次执行git_pull随机生成。
